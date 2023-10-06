61359b4d1c66cdf69d5e2e89b7cbb8abd15e18d1
gedeastu <gede.astu@gmail.com>
2023-10-07 02:48:29 +0700

    up

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import { Suspense, lazy } from 'react'
+import {
+  createBrowserRouter, 
+  Route,
+  createRoutesFromElements,
+  RouterProvider
+} 
+from 'react-router-dom'
 
+const RootLayout = lazy(()=>import('./layouts/RootLayout'));
+const WanitaLayout = lazy(()=>import('./layouts/'))
 const App = () => {
   return (
     <>
