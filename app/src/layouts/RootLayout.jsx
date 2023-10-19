5e3877c9e534564c7ee5df7485d5718fb6d70ba6
gedeastu <gede.astu@gmail.com>
2023-10-19 10:52:03 +0700

    up

--- app/src/layouts/RootLayout.jsx
+++ app/src/layouts/RootLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Search from '../components/Search'
+import Navfloat from '../components/Navfloat'
 const RootLayout = () => {
   return (
    <>
    <Search/>
    <Outlet/>
+   <Navfloat/>
    </>
   )
 }
