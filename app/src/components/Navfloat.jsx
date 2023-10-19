42c5e0b4ca5eaf20b634840dc0275e344ff86bc2
gedeastu <gede.astu@gmail.com>
2023-10-19 12:11:30 +0700

    up

--- app/src/components/Navfloat.jsx
+++ app/src/components/Navfloat.jsx
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom'
 import { navLink } from '../services/NavLink';
 const Navfloat = () => {
   const {isLoading, isError, data, error, isFetching, refetch} = navLink();
+  console.log(data)
   return (
     <nav>
     {data?.map((item)=>{
